fba12cee3cf6b2349d29299dc73a5bf52723b055
Arion <adrian.rybaczuk.praca@gmail.com>
2022-11-29 21:22:41 +0100

    Reformat file

--- lab8/alex.h
+++ lab8/alex.h
@@ -3,15 +3,24 @@
 
 #include <stdio.h>
 
-// interesujące leksemy: błąd, inny symbol, koniec pliku, otwierająca klamra {, 
-//                       zamykająca klamra }, identyfikator, otwierający nawias, 
+// interesujące leksemy: błąd, inny symbol, koniec pliku, otwierająca klamra {,
+//                       zamykająca klamra }, identyfikator, otwierający nawias,
 //                       zamykający nawias
-typedef enum { ERROR, OTHER, EOFILE, OPEBRA, CLOBRA, IDENT, OPEPAR, CLOPAR } lexem_t;
+typedef enum
+{
+    ERROR,
+    OTHER,
+    EOFILE,
+    OPEBRA,
+    CLOBRA,
+    IDENT,
+    OPEPAR,
+    CLOPAR
+} lexem_t;
 
-void    alex_init4file( FILE * );  // zacznij czytać nowy plik
-lexem_t alex_nextLexem( void );    // daj kolejny leksem w czytanym pliku
-char *  alex_ident( void );        // daj ostatni identyfikator
-int     alex_getLN();              // daj aktualny nr linii
+void alex_init4file(FILE *);  // zacznij czytać nowy plik
+lexem_t alex_nextLexem(void); // daj kolejny leksem w czytanym pliku
+char *alex_ident(void);       // daj ostatni identyfikator
+int alex_getLN();             // daj aktualny nr linii
 
 #endif
-
