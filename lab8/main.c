ecec532c580db03b5bc612cf32da9c731377c623
Arion <adrian.rybaczuk.praca@gmail.com>
2022-11-29 22:30:28 +0100

    file main.c  and requirements

---
+++ lab8/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fun_stack.h"
+#include "alex.h"
+#include "parser.h"
+
+int main(int argc, char **argv)
+{
+    // Zdefiniowanie rozmiaru dla struktury functionStack (fun_stack.h)
+    // Sprawdzenie działania fun_stack.c
+
+    // sprawdzenie działania alex.c
+
+    // Sprawdzenie działania stworzonej przez Ciebie struktury i funkcji do niej
+    // sprawdzenie działania parser.c
+
+    // Wczytanie plików z argv 
+}
