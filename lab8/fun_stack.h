2d121a0a1bea4aa8a43dc48108a546924782f6ef
ArionStudio <adrian.rybaczuk.praca@gmail.com>
2022-12-01 09:05:11 +0100

    Update fun_stack

--- lab8/fun_stack.h
+++ lab8/fun_stack.h
@@ -5,13 +5,15 @@ typedef struct e
 {
     char *name;
     int parLevel;
+    int braLevel;
     struct e *next;
+
 } *Node;
 
 Node *functionStack;
 
 int top_of_funstack(void);                          // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
-void put_on_fun_stack(int par_level, char *funame); // odkłada na stos parę (funame,par_level)
+void put_on_fun_stack(char *funame, int par_level, int bra_level); // odkłada na stos parę (funame,par_level)
 char *get_from_fun_stack(void);                     // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
 
 #endif
