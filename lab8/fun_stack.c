ccaf3e2b1dcd8276be8d70c8f14d6946dd837df9
Arion <adrian.rybaczuk.praca@gmail.com>
2022-12-02 14:43:36 +0100

    Fixes

--- lab8/fun_stack.c
+++ lab8/fun_stack.c
@@ -1,4 +1,7 @@
 #include "fun_stack.h"
+#include <stdlib.h>
+
+// Funckje struktury
 
 // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
 int top_of_funstack(void)
@@ -6,19 +9,31 @@ int top_of_funstack(void)
     return (*functionStack)->parLevel;
 }
 
-// odkłada na stos parę (funame, par_level)
+// odkłada na stos parę (funame, par_level, bra_level)
 void put_on_fun_stack(char *funame, int par_level, int bra_level)
+{
+    Node e = initNode(funame, par_level, bra_level);
+    e->next = *functionStack;
+    functionStack = &e;
+}
+
+// Pobiera ze stosu element zarazem go usuwając
+char *get_from_fun_stack(void)
+{
+    Node e = *functionStack;
+    *functionStack = (*functionStack)->next;
+    return e;
+}
+
+// Funkcje struktury Node
+
+// Inicjacja elementu stosu
+Node initNode(char *funame, int par_level, int bra_level)
 {
     Node e;
     e->name = funame;
     e->parLevel = par_level;
     e->braLevel = bra_level;
-    e->next = *functionStack;
-    functionStack = &e;
-}
-char *get_from_fun_stack(void)
-{
-    Node e = *functionStack;
-    *functionStack = (*functionStack)->next;
+    e->next = NULL;
     return e;
 }
--- lab8/fun_stack.h
+++ lab8/fun_stack.h
@@ -1,18 +1,30 @@
 #ifndef _FUN_STACK_H_IS_INCLUDED_
 #define _FUN_STACK_H_IS_INCLUDED_
 
+// Struktura przetrzymująca element stosu
 typedef struct e
 {
+    // Nazwa funkcji
     char *name;
+    // Zagłębienie nawiasowe
     int parLevel;
+    // Zagłębienie klamerkowe
     int braLevel;
+    // Wskaźnik na następny element
     struct e *next;
 } *Node;
 
 Node *functionStack;
 
-int top_of_funstack(void);                          // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
+// Funkcje dotyczące struktury Node
+
+// Inicjacja elementu stosu
+Node initNode(char *name, int parLevel, int braLevel);
+
+// Funkcje dotyczące strukty Stosu
+
+int top_of_funstack(void);                                         // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
 void put_on_fun_stack(char *funame, int par_level, int bra_level); // odkłada na stos parę (funame,par_level)
-char *get_from_fun_stack(void);                     // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
+char *get_from_fun_stack(void);                                    // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
 
 #endif
