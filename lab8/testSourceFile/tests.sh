2a1bfe4423672b662373da7afe016f9cb296ec89
Arion <adrian.rybaczuk.praca@gmail.com>
2022-12-08 00:42:25 +0100

    Repair make test and make test-repair command

--- lab8/testSourceFile/tests.sh
+++ lab8/testSourceFile/tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 files=""
-for FILE in $1/*.c do
-    files+=("$FILE")
+for FILE in $1/*.h 
+do
+    files+=" ${FILE}"
 done
-for FILE in $1/*.h do 
-	files=$file $FILE
+for FILE in $1/*.c 
+do
+    files+=" ${FILE}"
 done
-echo $files
+
+./lib/a.out $files
