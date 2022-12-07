1c99ea68f0103d6b2dc663abcc020171dbe9c8cf
ArionStudio <adrian.rybaczuk.praca@gmail.com>
2022-12-08 00:30:57 +0100

    Last version from unv of sh test script

--- lab8/testSourceFile/tests.sh
+++ lab8/testSourceFile/tests.sh
@@ -1,8 +1,9 @@
-# files=""
-# for FILE in $1/*.c ; do \
-# 	$files += "${FILE} " ; \
-# done
-# for FILE in $1/*.h ; do \
-# 	$files += "${FILE}" ; \
-# done
-# $1/test.out $files
+#!/bin/bash
+files=""
+for FILE in $1/*.c do
+    files+=("$FILE")
+done
+for FILE in $1/*.h do 
+	files=$file $FILE
+done
+echo $files
