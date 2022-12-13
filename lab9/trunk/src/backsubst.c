21323320ecb1c8e42fc389b757818f0592acf6f4
Arion <arion@DESKTOP-C2LRK4E.localdomain>
2022-12-13 21:04:47 +0100

    Add backsubst function

--- lab9/trunk/src/backsubst.c
+++ lab9/trunk/src/backsubst.c
@@ -4,19 +4,18 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+void backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    /**
+     * Tutaj należy umieścić właściwą implemntację.
+     */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+    int n = mat->r-1;
+	double sum = 0.0;
+	x->data[n][0] = b->data[n][0] / mat->data[n][n];
+	for(int i = n - 1; i >= 0; i--){
+		sum = 0;
+		for(int j = i + 1; j <= n; j++)
+			sum += mat->data[i][j]*x->data[j][0];
+		x->data[i][0] = (b->data[i][0] - sum ) / mat->data[i][i];
+	}
 }
-
-
